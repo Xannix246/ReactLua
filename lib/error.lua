db7e9bbe0faed3b647a113774ed74615f8723077
Xannix246 <albertohrimenko@gmail.com>
2022-12-14 10:36:27 +0300

    Update error.lua

--- lib/error.lua
+++ lib/error.lua
@@ -6,7 +6,7 @@ local error={}
 
 function error.set(err)
     GUIBackground.GUIError()
-    gpu.setBackground(0x000000)
+    gpu.setForeground(0x000000)
     gpu.set((width/2-w+3), (height/2)-h*0.4, "Error: " .. err)
 end
 
