63783d29f82c27e3acbe014b66e267965e8d8677
Xannix246 <albertohrimenko@gmail.com>
2022-12-14 10:39:32 +0300

    Update error.lua

--- lib/error.lua
+++ lib/error.lua
@@ -2,12 +2,12 @@ local GUIBackground = require('../lib/GUIBackground')
 local gpu = component.gpu
 local width, height = gpu.getResolution()
 
-local error={}
+local err={}
 
-function error.set(err)
+function err.set(err1)
     GUIBackground.GUIError()
     gpu.setForeground(0x000000)
-    gpu.set((width/2-w+3), (height/2)-h*0.4, "Error: " .. err)
+    gpu.set((width/2-w+3), (height/2)-h*0.4, "Error: " .. err1)
 end
 
-return error
+return err
