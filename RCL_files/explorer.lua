5b67f02c2ce5fc10cfc8e0e7c5ba195bd6e5946b
Xannix246 <62834423+Xannix246@users.noreply.github.com>
2022-12-15 23:32:16 +0300

    1

--- RCL_files/explorer.lua
+++ RCL_files/explorer.lua
@@ -4,7 +4,7 @@ require('../lib/program')
 
 local explorer = {}
 
-local function explorer()
+function explorer.set()
     gpu.setForeground(0x000000)
     gpu.fill(x, y, w, h, " ")
 end
--- autorun.lua
+++ autorun.lua
@@ -4,6 +4,6 @@ require('db/autorun_files')
 function autorun_p()
     for i=1, #autorun do
         local program = require('RCL_files/' .. autorun[i])
-        program.autorun[i]()
+        program.set()
     end
 end
