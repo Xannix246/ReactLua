f445ae5bb41556d69fd49442aeb90df864c10985
AistopGit <106827114+AistopGit@users.noreply.github.com>
2022-12-18 14:47:13 +0300

    fuck notepad x2

--- installer.lua
+++ installer.lua
@@ -45,8 +45,8 @@ gpu.set(7, 15, "             ╚════════════════
 while firstStage == true do
     local key = {event.pull("key_down")} 
     if key[4] == 46 then
-        term.clear()
-        firstStage = false
+		term.clear()
+		firstStage = false
 
     elseif key[4] == 16 then
         term.clear()
@@ -69,7 +69,7 @@ while secondStage == true do
     local key = {event.pull("key_down")} 
     if key[4] == 46 then
         term.clear()
-        secondStage = false
+		secondStage = false
 
     elseif key[4] == 16 then
         term.clear()
