7d5a104b6783167c7957da8eeb7a96790498b838
Xannix246 <albertohrimenko@gmail.com>
2022-12-27 23:10:28 +0300

    Update exp_buttons.lua

--- RCL_files/explorer/db_exp/exp_buttons.lua
+++ RCL_files/explorer/db_exp/exp_buttons.lua
@@ -35,6 +35,11 @@ _G.exp_btn = {
             local function buttons(n)
                 button.set(btn[n].X, btn[n].Y, btn[n].W, btn[n].H, btn[n].text, btn[n].textColor, btn[n].color)
             end
+            
+            for i=1, #btn do
+                buttons(i)
+            end
+
         end
     }
 }
